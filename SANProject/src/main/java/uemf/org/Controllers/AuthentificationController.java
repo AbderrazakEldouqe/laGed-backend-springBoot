523a1f4b812b2294c2af085ba183e36cf59e205a
HanaeBerouylat <hanae.berouylat@cgi.com>
2021-08-30 11:22:59 +0100

    add requestBody to auth

--- SANProject/src/main/java/uemf/org/Controllers/AuthentificationController.java
+++ SANProject/src/main/java/uemf/org/Controllers/AuthentificationController.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.Tag;
 import uemf.org.Models.CustomUserDetailsDTO;
+import uemf.org.Requests.AuthRequest;
 import uemf.org.Services.AuthentificationService;
 
 
@@ -31,9 +32,9 @@ public class AuthentificationController {
 	
 	@ApiOperation(value = "auth User")
 	@PostMapping("/authUser")
-	public ResponseEntity<CustomUserDetailsDTO> authUser(@RequestParam("login") String login, @RequestParam("passWord") String passWord )
+	public ResponseEntity<CustomUserDetailsDTO> authUser(@RequestBody AuthRequest authRequest )
 	{	
-		return  new ResponseEntity<CustomUserDetailsDTO>(authentificationService.authUser(login, passWord), HttpStatus.OK) ;
+		return  new ResponseEntity<CustomUserDetailsDTO>(authentificationService.authUser(authRequest.getLogin(), authRequest.getPassWord()), HttpStatus.OK) ;
 	   
 	}
 	

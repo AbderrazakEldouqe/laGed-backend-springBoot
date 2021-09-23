11aa37e58963a901cec5266d591b21aed16a9c10
HanaeBerouylat <hanae.berouylat@gmail.com>
2021-09-23 11:45:31 +0100

    add external file properties

--- SANProject/src/main/java/uemf/org/SanProjectApplication.java
+++ SANProject/src/main/java/uemf/org/SanProjectApplication.java
@@ -2,8 +2,23 @@ package uemf.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+
+
+
 
 @SpringBootApplication
+
+/*                    external property sources    */
+
+/*
+@PropertySources({
+  @PropertySource("classpath:application.properties")
+  , @PropertySource(value = "file:C:\\Users\\hindberouyat\\OneDrive\\Bureau\\Hanae Berouylat\\CompileTest\\application.properties",
+  ignoreResourceNotFound = true)
+}) */
 public class SanProjectApplication {
 
 	public static void main(String[] args) {
